505d7d479ec0c761029e6e484a6962a036feabbe
nybah <botosarpad@gmail.com>
2021-08-31 12:52:34 +0200

    Added html skeleton for login and registration pages.

--- app.js
+++ app.js
@@ -14,4 +14,9 @@ app.get('/login', function (req, res) {
     res.render('login.pug')
 })
 
+app.use(express.static('public'));
+app.get('/register', function (req, res) {
+    res.render('register.pug')
+})
+
 app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
