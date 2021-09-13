da14fb371c073aa9c17ae3a77e1b94e11721c08c
nybah <botosarpad@gmail.com>
2021-09-13 12:51:51 +0200

    Added placeholder functionality for sitting and standing in rooms. (To be implemented with web sockets later on)

--- public/room.js
+++ public/room.js
@@ -1,10 +1,90 @@
 var roomName;
+var username;
 
-window.onload = function roomOnLoad() {
+window.onload = async function roomOnLoad() {
 	const urlParams = new URLSearchParams(window.location.search);
 	roomName = urlParams.get("name");
-	roomHeader = document.getElementById("roomHeader");
-	roomHeader.innerHTML = "Room " + roomName;
+
+	if (localStorage.getItem("tandem-token") === null) {
+		redirectToLogin();
+	} else {
+		username = await getUsername();
+	}
+
+	const response = await queryRoom();
+	if (response.ok) {
+		const roomHeader = document.getElementById("roomHeader");
+		roomHeader.innerHTML = "Room " + roomName;
+		const roomInfo = await parsePositions(response);
+		const tbody = document.getElementById("positionTableBody");
+		fillTable(tbody, roomInfo.players);
+	} else {
+		redirectToLogin();
+	}
+}
+
+function redirectToLogin() {
+	location.href = "http://localhost:3000/login?redirectedFrom=room&name=" + roomName;
+}
+
+async function getUsername() {
+	const response = await fetch("http://localhost:8080/user", {
+		method: 'GET',
+		headers: {
+			'tandem-token': localStorage.getItem("tandem-token"),
+			'Content-Type': 'application/json'
+	}});
+	const userInfo = await response.json();
+	return userInfo.username;
+}
+
+async function queryRoom() {
+	const response = await fetch("http://localhost:8080/room?room=" + roomName, {
+		method: 'GET',
+		headers: {
+			'tandem-token': localStorage.getItem("tandem-token"),
+			'Content-Type': 'application/json'
+	}});
+	return response;
+}
+
+function parsePositions(response) {
+	const roomInfo = response.json();
+	return roomInfo;
+}
+
+function fillTable(tbody, players) {
+	console.log(players);
+	const rowWhite1 = tbody.insertRow();
+	const rowBlack1 = tbody.insertRow();
+	const rowWhite2 = tbody.insertRow();
+	const rowBlack2 = tbody.insertRow();
+	rowWhite1.insertCell().innerHTML = "White 1";
+	rowBlack1.insertCell().innerHTML = "Black 1";
+	rowWhite2.insertCell().innerHTML = "White 2";
+	rowBlack2.insertCell().innerHTML = "Black 2";
+	fillPlayer(rowWhite1, "WHITE1", players);
+	fillPlayer(rowBlack1, "BLACK1", players);
+	fillPlayer(rowWhite2, "WHITE2", players);
+	fillPlayer(rowBlack2, "BLACK2", players);
+}
+
+function fillPlayer(row, position, players) {
+	if (players[position]) {
+		if (players[position] == username) {
+			const button = document.createElement("button");
+			button.innerHTML = "Stand";
+			button.onclick = function () { stand(position, button); }
+			row.insertCell().appendChild(button);
+		} else {
+			row.insertCell().innerHTML = players[position];
+		}
+	} else {
+		const button = document.createElement("button");
+		button.innerHTML = "Sit";
+		button.onclick = function () { sit(position, button); }
+		row.insertCell().appendChild(button);
+	}
 }
 
 async function leaveRoom() {
@@ -16,4 +96,34 @@ async function leaveRoom() {
 	}});
 	const responseJson = await response.json();
 	location.href = "http://localhost:3000/lobby";
+}
+
+async function sit(position, button) {
+	const response = await fetch("http://localhost:8080/room/sit?room=" + roomName + "&position=" + position, {
+		method: 'POST',
+		headers: {
+			'tandem-token': localStorage.getItem("tandem-token"),
+			'Content-Type': 'application/json'
+	}});
+	if (response.ok) {
+		button.innerHTML = "Stand";
+		button.onclick = function() { stand(position, button); }
+	} else {
+		// TODO
+	}
+}
+
+async function stand(position, button) {
+	const response = await fetch("http://localhost:8080/room/stand", {
+		method: 'POST',
+		headers: {
+			'tandem-token': localStorage.getItem("tandem-token"),
+			'Content-Type': 'application/json'
+	}});
+	if (response.ok) {
+		button.innerHTML = "Sit";
+		button.onclick = function() { sit(position, button); }
+	} else {
+		// TODO
+	}
 }
